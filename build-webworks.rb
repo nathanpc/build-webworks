e942c20659848f13e332fd29253f6c0bfe97af9b
Nathan Campos <eeepc904@gmail.com>
2012-12-07 01:08:29 -0200

    Now using system instead of exec, also implemented some error messages

--- build-webworks.rb
+++ build-webworks.rb
@@ -50,11 +50,6 @@ def clean
     FileUtils.rm_r File.join(Dir.pwd, "build/"), :force => true
 end
 
-def build_zip(device)
-    Dir.mkdir File.join(Dir.pwd, "build/")
-    exec "zip -r build/#{device}.zip * -x build/ build.json"
-end
-
 def check_build
     id_file = File.join(Dir.pwd, ".buildid")
     build_id = 1
@@ -76,22 +71,31 @@ def check_build
     return build_id
 end
 
+def generate_zip(device)
+    puts "Generating archive".bold
+
+    clean()
+    Dir.mkdir File.join(Dir.pwd, "build/")
+    unless system "zip -r build/#{device}.zip * -x build/ build.json"
+        abort "An error ocurried while trying to generate the archive".red.bold
+    end
+end
+
 def build(device, option, config)
     sdk = config[device]["sdk"]
     build_id = 1
 
-    puts "Generating archive".bold
-    clean()
-    build_zip device
-
-    puts "\nCompiling BAR".bold
+    puts "Compiling BAR".bold
     build_id = check_build()
-    exec "#{File.join(sdk["location"], "bbwp")} build/#{device}.zip -buildId #{build_id} -o build/"
+    unless system "#{File.join(sdk["location"], "bbwp")} build/#{device}.zip -buildId #{build_id} -o build/"
+        abort "An error ocurried while trying to compile the BAR".red.bold
+    end
 end
 
 def parse_params(command, device, option, config)
     case command
         when "build"
+            generate_zip device
             build device, option, config
         when "sign"
         when "deploy"
